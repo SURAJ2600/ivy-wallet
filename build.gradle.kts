cebbd5f469429c168ee1c64070d89aab9fa7f383
Suraj <Suraj2600@gmail.com>
2023-10-19 15:30:11 +0800

    feat : add kover code coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.internal.impldep.org.testng.reporters.XMLUtils.xml
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     // Run with:
@@ -6,7 +8,39 @@ plugins {
     id("ivy.detekt")
 
     alias(libs.plugins.gradleWrapperUpgrade)
+
+    alias(libs.plugins.koverPlugin)
 }
+
+
+subprojects {
+
+
+}
+subprojects {
+    apply(plugin = "org.jetbrains.kotlinx.kover")
+    koverReport {
+        // filters for all report types of all build variants
+        filters {
+            excludes {
+                classes(
+                    "*Activity",
+                    "*Activity\$*",
+                    "*.BuildConfig",
+                    "dagger.hilt.*",
+                    "hilt_aggregated_deps.*",
+                    "*.Hilt_*"
+                )
+                annotatedBy("@Composable")
+            }
+        }
+    }
+
+}
+
+
+
+
 
 wrapperUpgrade {
     gradle {
